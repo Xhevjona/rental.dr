ca81e087e1c63b85d7c0e66db9c7b34b12d4c148
Xhevjona <xhevjonamuca@gmail.com>
2024-04-14 20:36:09 +0200

    navbar fix

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-import '../Layout/header.scss';
+import React, { useState } from "react";
+import "../Layout/header.scss";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <nav className="responsive-navbar">
-        <div className="navbar-brand">
-          <button className={isOpen ? "navbar-toggler open" : "navbar-toggler"} onClick={toggleMenu}>
-            <span className="navbar-toggler-icon"></span>
-            <span className="navbar-toggler-icon"></span>
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="logo"><h3> Rental.Dr </h3></div>
-        </div>
-        <ul className={isOpen ? "navbar-nav open" : "navbar-nav"}>
-          <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="/about" className="nav-link">About</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Contact</a></li>
-        </ul>
-      </nav>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
-  
+
+  return (
+    <nav className="responsive-navbar">
+      <div className="navbar-brand">
+        <button
+          className={isOpen ? "navbar-toggler open" : "navbar-toggler"}
+          onClick={toggleMenu}
+        >
+          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="logo">
+          <h3> Rental.Dr </h3>
+        </div>
+      </div>
+      <ul className={isOpen ? "navbar-nav open" : "navbar-nav"}>
+        <li className="nav-item">
+          {" "}
+          <NavLink to="/" className="nav-link">
+            {" "}
+            Home
+          </NavLink>{" "}
+        </li>
+        <li className="nav-item">
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" className="nav-link">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
